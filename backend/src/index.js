dcc9da97705dc9cca95acbd5c176e26989bcfdc4
Roshan Binoj <roshanbinoj2005@gmail.com>
2025-10-23 16:49:33 +0530

    Messaging template done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on PORT:" + PORT);
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
